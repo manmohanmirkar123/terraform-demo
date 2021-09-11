0ad60bdf35b3be58df25ebe59789811b39b8da6e
manmohanmirkaremx <manmohan.mirkar@emxcelsolutions.com>
2021-09-11 17:26:02 +0530

    added variables 2nd lab with TFvars example

---
+++ variable-lab2-tfvars/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+    region = "us-east-1"
+
+}
+
+resource "aws_instance" "my-instance-1" {
+  instance_type = var.instance_type
+  ami = var.ami
+  tags = {
+      Name = "ec2_instance_1"
+      env = "dev"
+  }
+}
+
+resource "aws_instance" "my-instance-2" {
+  instance_type = var.instance_type
+  ami = var.ami
+  tags = {
+      Name = "ec2_instance_2"
+      env = "qa"
+  }
+}
+
