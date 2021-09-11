7d1b80d80bfc7c6f176134078b7b446681bd1403
manmohanmirkaremx <manmohan.mirkar@emxcelsolutions.com>
2021-09-11 11:32:49 +0530

    added tags in win2019 ec2 launch script

--- chapter4/exe1/win_EC2.tf
+++ chapter4/exe1/win_EC2.tf
@@ -1,3 +1,5 @@
+
+#This script will be used for deploying win2019 ami in AWS
 provider "aws" {
   region = "us-east-1"
 }
@@ -5,5 +7,7 @@ provider "aws" {
 resource "aws_instance" "win_ec2" {
     ami = "ami-02cc00a60f7d8adb7"
     instance_type = "t2.micro"
-  
+    tags = {
+      "Name" = "Win_EC2"
+    }
 }
